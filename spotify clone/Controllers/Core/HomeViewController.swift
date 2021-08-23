c9339ae695b14f0c541326ce197e1ffba8afd896
Igor Morozov <24278723+morozoffnor@users.noreply.github.com>
2021-08-24 02:41:37 +0300

    added a gear icon leading to ProfileViewController
    it will be eventually changed to SettingsViewController but it's fine for now

--- spotify clone/Controllers/Core/HomeViewController.swift
+++ spotify clone/Controllers/Core/HomeViewController.swift
@@ -15,8 +15,14 @@ class HomeViewController: UIViewController {
         title = "Home"
         // view background color
         view.backgroundColor = .systemBackground
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTapSettings))
     }
 
+    @objc func didTapSettings() {
+        let vc = ProfileViewController()
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
 
