639f6b9623a64db9beaf3e52715ee432d78b6f98
Igor Morozov <24278723+morozoffnor@users.noreply.github.com>
2021-08-24 02:44:40 +0300

    createRequest func and getCurrentProfile func
    - added a func that will create a base request with all the parameters needed to "buld" any request on top of it
    - added getCurrentProfile func but it does like nothing for now because i haven't written a UserProfile model yet

--- spotify clone/Managers/APICaller.swift
+++ spotify clone/Managers/APICaller.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    private init() {}
+    
+    struct Constants {
+        static let baseApiUrl = "https://api.spotify.com/v1"
+    }
+    
+    enum apiError: Error {
+        case failedToGetData
+    }
+    public func getCurrentUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void) {
+        createRequest(with: URL(string: Constants.baseApiUrl + "/me"), type: .GET) { baseRequest in
+            // get data
+            let task = URLSession.shared.dataTask(with: baseRequest) { data, _ , error in
+                guard let data = data, error == nil else {
+                    // if failed
+                    completion(.failure(apiError.failedToGetData))
+                    return
+                }
+                
+                do {
+                    let result = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                    print(result)
+                }
+                catch {
+                    completion(.failure(error))
+                }
+                
+            }
+            task.resume()
+        }
+    }
+    
+    // enum for http methods
+    enum Method: String {
+        case GET
+        case POST
+    }
+    
+    // create a generic request which will be used for everything else to build on top of
+    private func createRequest(with url: URL?, type: Method, completion: @escaping (URLRequest) -> Void) {
+        AuthManager.shared.withValidToken { token in
+            guard let apiURL = url else {
+                return
+            }
+            var request = URLRequest(url: apiURL)
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+            request.httpMethod = type.rawValue
+            request.timeoutInterval = 30
+            
+            completion(request)
+        }
+        
+    }
+}
