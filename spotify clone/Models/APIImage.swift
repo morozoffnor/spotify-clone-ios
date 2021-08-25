05dcf7d131ff4df16ca9b2cc3d678fa6334bc070
Igor Morozov <24278723+morozoffnor@users.noreply.github.com>
2021-08-25 19:25:54 +0300

    Models
    Created a bunch of models:
    - APIImage model
    - Artist model
    - NewReleasesResponse model
    - changed UserProfile model to use APIImage model

---
+++ spotify clone/Models/APIImage.swift
@@ -0,0 +1,14 @@
+//
+//  APIImage.swift
+//  spotify clone
+//
+//  Created by Игорь Морозов on 25.08.2021.
+//
+
+import Foundation
+
+struct APIImage: Codable {
+    let url: String
+    let height: Int?
+    let width: Int?
+}
---
+++ spotify clone/Models/NewReleasesResponse.swift
@@ -0,0 +1,29 @@
+//
+//  NewReleasesResponse.swift
+//  spotify clone
+//
+//  Created by Игорь Морозов on 25.08.2021.
+//
+
+import Foundation
+
+struct NewReleasesResponse: Codable {
+    let albums: AlbumsResponse
+}
+
+struct AlbumsResponse: Codable {
+    let items: [Album]
+}
+
+struct Album: Codable {
+    let album_type: String
+    let available_markets: [String]
+    let id: String
+    let images: [APIImage]
+    let name: String
+    let release_date: String
+    let total_tracks: Int
+    let uri: String
+    let artists: [Artist]
+}
+
